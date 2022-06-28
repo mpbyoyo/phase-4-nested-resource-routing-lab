fafdd30abd87fbf04d808333977f83fed051e5fa
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-06-28 17:07:58 -0500

    commit tree 10a55e7d321bb706300d49a184dbbfd5ff8c14ce

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items
+    else 
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+      render json: item
+    else 
+      item = Item.find(params[:id])
+      render json: item
+    end
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response
+    render json: { error: "Item(s) not found" }, status: :not_found
   end
 
 end
